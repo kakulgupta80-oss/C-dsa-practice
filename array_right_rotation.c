f44976d2c9a57da5fc89b885a33c3cd507b961c4
kakulgupta80-oss <kakulgupta80@gmail.com>
2026-03-27 12:03:10 +0530

    Create array_right_rotation.c

---
+++ array_right_rotation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, r;
+    int arr[100];  
+
+    printf("Enter the size of array:\n");
+    scanf("%d", &n);
+
+    printf("Enter the elements of array:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value of k:\n");
+    scanf("%d", &k);
+    k = k % n;  
+
+    int temp;
+    for(r = 0; r < k; r++) {
+        temp = arr[0];
+        for(i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = temp;
+    }
+
+    printf("After left rotation:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+
+    return 0;
+}
+
